f680e0e93562ed47ad0a4309762001dbe163e10b
hiroro <hrsi.ad@gmail.com>
2022-03-23 13:29:01 +0900

    Initial Commit

---
+++ Tinder/TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderApp.swift
+//  Tinder
+//
+//  Created by Hiroshi.Nakai on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
